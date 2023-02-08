58a75a8752a3ee9ef9a8d1b33088783cccbd3660
LottaMe <56435437+LottaMe@users.noreply.github.com>
2023-02-08 16:40:18 +0100

    add templates

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello Index Page'
+    user = {'username': 'Brenden'}
+    recipes = [
+        {
+            'author': {'username': 'Lotta'},
+            'title': 'Cooking... Yay'
+        },
+        {
+            'author': {'username': 'Brenden'},
+            'title': 'Peanut Butter... It just peanuts'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, recipes=recipes)
